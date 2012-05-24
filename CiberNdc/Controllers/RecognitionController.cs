21c0dfb256d9b868bbb01cfbbb6dd2ac46b442f0
Samson Valland <samsonvalland@gmail.com>
2012-05-24 15:02:48 +0200

    Revert "Bildeorientation på iphone skapar trøbbel"
    This reverts commit 4cc8993ceda75215e93c78593a6bb6f0496c44d0.

--- CiberNdc/Controllers/RecognitionController.cs
+++ CiberNdc/Controllers/RecognitionController.cs
@@ -32,8 +32,6 @@ namespace CiberNdc.Controllers
             {
                 tids.AddRange(p.Tags.Select(t => t.tid));
             }
-            if (tids.Count == 0)
-                return null;
             _api.TagsSave(tids, uids.FirstOrDefault(), null, null);
             var traint = _api.FacesTrain(uids, "ndcwebapp.apphb.com", "");
 
