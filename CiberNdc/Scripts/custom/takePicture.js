84d40cb42e7224b87a0524df1128b9a81dd56acf
Samson Valland <samsonvalland@gmail.com>
2012-06-01 22:18:26 +0200

    videoInit fix

--- CiberNdc/Scripts/custom/takePicture.js
+++ CiberNdc/Scripts/custom/takePicture.js
@@ -8,11 +8,11 @@
     },
 
     update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-
+        computeSize(true);
         var canvas = $('canvas').get(0);
         var video = $('video').get(0);
         //var img = $('img').get(0);
-        
+
         var ctx = canvas.getContext('2d');
         if (viewModel.stream) {
             ctx.drawImage(video, 0, 0, 300, 300);
--- CiberNdc/Scripts/custom/videoInit.js
+++ CiberNdc/Scripts/custom/videoInit.js
@@ -1,11 +1,45 @@
-﻿ko.bindingHandlers.videoInit = {
+﻿var VIDEO_WIDTH, VIDEO_HEIGHT,
+computeSize = function(supportsObjectFit) {
+	// user agents that don't support object-fit 
+	// will display the video with a different 
+	// aspect ratio. 
+	if (supportsObjectFit === true) {
+		VIDEO_WIDTH = 640;
+		VIDEO_HEIGHT = 480;
+	} else {
+		VIDEO_WIDTH = video.videoWidth;
+		VIDEO_HEIGHT = video.videoHeight;
+	}
+},
+
+successCallback = function (stream) {
+	video.src = stream;
+	video.play();
+	computeSize(true);
+},
+errorCallback = function (error) {
+	console.error('An error occurred: [CODE ' + error.code + ']');
+	computeSize(true);
+};
+
+ko.bindingHandlers.videoInit = {
     init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
         if (navigator.getUserMedia) {
             navigator.getUserMedia({ 'video': true, 'audio': false }, function (raw) {
 
                 viewModel.stream = raw;
                 element.src = raw;
+                computeSize(true);
+            }, errorCallback);
+        } else if (navigator.webkitGetUserMedia) {
+
+            navigator.webkitGetUserMedia("video", function (stream) {
+                viewModel.stream = stream;
+                element.src = window.webkitURL.createObjectURL(stream);
+
+                computeSize(false);
             });
+
         } else {
             alert("Your browser does not support access to your camera, try using the latest version of Opera Mobile!");
         }
