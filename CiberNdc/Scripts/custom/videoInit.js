bb4b0b4ab350202ed2c8075b761cf5e2c03a787b
Samson Valland <samsonvalland@gmail.com>
2012-06-03 17:48:25 +0200

    skalering fiksa?

--- CiberNdc/Scripts/custom/videoInit.js
+++ CiberNdc/Scripts/custom/videoInit.js
@@ -1,19 +1,19 @@
 ﻿var VIDEO_WIDTH, VIDEO_HEIGHT,
 computeSize = function (supportsObjectFit) {
-    // user agents that don't support object-fit 
-    // will display the video with a different 
-    // aspect ratio. 
+	// user agents that don't support object-fit 
+	// will display the video with a different 
+	// aspect ratio. 
 
-    var width = $(".purple-square").width();
-    var height = $(".purple-square").height();
-    var aspect = width / height;
-    console.log("aspect: " + aspect + "(" + width + "/" + height +")");
-    
-    if (aspect < 1) {
-        video.height = $(".purple-square").height();
-    } else {
-        video.width = $(".purple-square").width();
-    }
+	var width = $(".purple-square").width();
+	var height = $(".purple-square").height();
+	var aspect = width / height;
+	console.log("aspect: " + aspect + "(" + width + "/" + height +")");
+	
+	if (aspect < 1) {
+		video.height = $(".purple-square").height();
+	} else {
+		video.width = $(".purple-square").width();
+	}
 
 },
 
@@ -28,24 +28,24 @@ errorCallback = function (error) {
 };
 
 ko.bindingHandlers.videoInit = {
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        if (navigator.getUserMedia) {
-            navigator.getUserMedia({ 'video': true, 'audio': false }, function (raw) {
-                viewModel.stream = raw;
-                element.src = raw;
-                computeSize(true);
-            }, errorCallback);
-        } else if (navigator.webkitGetUserMedia) {
+	init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia({ 'video': true, 'audio': false }, function (raw) {
+				viewModel.stream = raw;
+				element.src = raw;
+				computeSize(true);
+			}, errorCallback);
+		} else if (navigator.webkitGetUserMedia) {
 
-            navigator.webkitGetUserMedia("video", function (stream) {
-                viewModel.stream = stream;
-                element.src = window.webkitURL.createObjectURL(stream);
-                computeSize(false);
-            });
+			navigator.webkitGetUserMedia("video", function (stream) {
+				viewModel.stream = stream;
+				element.src = window.webkitURL.createObjectURL(stream);
+				computeSize(false);
+			});
 
-        } else {
-            alert("Your browser does not support access to your camera, try using the latest version of Opera Mobile, or activate it in Chrome by setting the MediaStream-flag in chrome://flags/");
-        }
-    }
+		} else {
+			alert("Din nettleser støtter ikke bruk av kameraet, prøv å bruke siste versjon av Opera Mobile!");
+		}
+	}
 };
 
